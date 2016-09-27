580112afddb0de0fa2d868ea979d3a88e57881d2
jlengstorf <jason.lengstorf@copterlabs.com>
2016-09-27 15:41:23 -0700

    feat(dev): easier dev environment

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import eslint from 'rollup-plugin-eslint';
+
+export default {
+  entry: 'src/scripts/main.js',
+  dest: 'static/js/main.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  external: [
+    '$',
+    'Handlebars',
+  ],
+  plugins: [
+    eslint({
+      exlude: [
+        'src/styles/**',
+      ],
+      globals: [
+        '$',
+        'Handlebars',
+      ],
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
+  ],
+};
