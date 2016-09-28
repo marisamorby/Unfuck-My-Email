ea27e7342cc4ffbf0a66233b9c499ce90a6858b1
jlengstorf <jason.lengstorf@copterlabs.com>
2016-09-27 22:59:09 -0700

    feat(forms): no-jQuery form-to-JSON
    See https://code.lengstorf.com/get-form-values-as-json/ for how
    it works.

--- src/scripts/lib/forms.js
+++ src/scripts/lib/forms.js
@@ -1,16 +1,88 @@
-/**
- * Choose the correct input template based on the type.
- * See http://handlebarsjs.com/#helpers for more info.
- *
- * @return {String} the rendered template.
- */
-export function renderInput() {
-  var textInput = Handlebars.compile($('#input-text').html());
-  var radioInput = Handlebars.compile($('#input-radio').html());
+import { getEmailTemplate, removeBreakTags } from './templates';
 
-  return this.type === 'radio' ? radioInput(this) : textInput(this);
-}
+/**
+ * Checks that an element has a non-empty `name` and `value` property.
+ * @param  {Element} element  the element to check
+ * @return {Bool}             true if the element is an input, false if not
+ */
+const isValidElement = element => {
+  return element.name && element.value;
+};
+
+/**
+ * Checks if an element’s value can be saved (e.g. not an unselected checkbox).
+ * @param  {Element} element  the element to check
+ * @return {Boolean}          true if the value should be added, false if not
+ */
+const isValidValue = element => {
+  return (!['checkbox', 'radio'].includes(element.type) || element.checked);
+};
+
+/**
+ * Checks if an input is a checkbox, because checkboxes allow multiple values.
+ * @param  {Element} element  the element to check
+ * @return {Boolean}          true if the element is a checkbox, false if not
+ */
+const isCheckbox = element => element.type === 'checkbox';
+
+/**
+ * Checks if an input is a `select` with the `multiple` attribute.
+ * @param  {Element} element  the element to check
+ * @return {Boolean}          true if the element is a multiselect, false if not
+ */
+const isMultiSelect = element => element.options && element.multiple;
+
+/**
+ * Retrieves the selected options from a multi-select as an array.
+ * @param  {HTMLOptionsCollection} options  the options for the select
+ * @return {Array}                          an array of selected option values
+ */
+const getSelectValues = options => [].reduce.call(options, (values, option) => {
+  return option.selected ? values.concat(option.value) : values;
+}, []);
+
+/**
+ * Retrieves input data from a form and returns it as a JSON object.
+ * @param  {HTMLFormControlsCollection} elements  the form elements
+ * @return {Object}                               form data as an object literal
+ */
+const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+
+  // Make sure the element has the required properties and should be added.
+  if (isValidElement(element) && isValidValue(element)) {
+
+    /*
+     * Some fields allow for more than one value, so we need to check if this
+     * is one of those fields and, if so, store the values as an array.
+     */
+    if (isCheckbox(element)) {
+      data[element.name] = (data[element.name] || []).concat(element.value);
+    } else if (isMultiSelect(element)) {
+      data[element.name] = getSelectValues(element);
+    } else {
+      data[element.name] = element.value;
+    }
+  }
+
+  return data;
+}, {});
 
 export function filterFormsByCategory(forms, category) {
   return forms.filter(form => form.category === category);
 }
+
+export function handleSubmit(event) {
+  event.preventDefault();
+
+  const templateDisplayBox = document.querySelector('.template-text');
+  const copyPasteTextarea = document.querySelector('.copy-paste');
+
+  // Get the user’s form input and use it to compile the email template.
+  const templateVars = formToJSON(event.target.elements);
+  const compile = getEmailTemplate(templateVars.template_id);
+  const compiledTemplate = compile(templateVars);
+
+  templateDisplayBox.innerHTML = compiledTemplate;
+  copyPasteTextarea.value = removeBreakTags(compiledTemplate);
+}
+
